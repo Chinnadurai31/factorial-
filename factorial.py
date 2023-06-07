3254b86ea74b1ef2091dc8a5b1639d704e0ebe0d
Chinnadurai31 <135804225+Chinnadurai31@users.noreply.github.com>
2023-06-07 12:06:03 +0530

    user input is given to factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,8 @@
+user = int(input('enter range:'))
 result = 1
 
-for number in range(1, 101):
+for number in range(1, user+1):
     result *= number
     print("Factorial of", number, "is", result)
+    
 
